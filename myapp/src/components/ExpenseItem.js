ab20e506914d050bcfe8f93deb281d77c13b8df4
shema jolivet gislain <shemajolivet@gmail.com>
2022-10-28 16:43:55 +0200

    adding components css file

--- myapp/src/components/ExpenseItem.js
+++ myapp/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+        <div>
+            <div>march 28th 2021</div>
+             <div>
+                <h2>Car Insurance</h2>
+                <div>$2131.12</div>
+             </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
