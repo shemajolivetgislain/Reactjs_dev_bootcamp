2c0469d8291014ebad4810d9d22d7ea772aaf968
shema jolivet gislain <shemajolivet@gmail.com>
2022-10-29 09:40:23 +0200

    Expense Date component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -11,13 +11,13 @@ function App() {
       id: "e1",
       title: "Laptop",
       amount: 194.12,
-      date: new Date(2021, 7, 21),
+      date: new Date(2021, 3, 21),
     },
     {
       id: "e1",
       title: "Smart phone",
       amount: 123.12,
-      date: new Date(2022, 7, 21),
+      date: new Date(2022, 10, 21),
     },
   ];
   return (
--- myapp/src/components/ExpenseItem.js
+++ myapp/src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
 import './Expenseitem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-us', {month: 'short'});
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
-  const year = props.date.getFullYear();
     return (
       <div className="expense-item">
-        <div>
-          <div>{month}</div>
-          <div>{year}</div>
-          <div>{day}</div>
-        </div>
+        <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">{props.amount}</div>
